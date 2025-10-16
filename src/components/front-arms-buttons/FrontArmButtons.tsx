74d5289518e15d0e91d1488b587cbdd50cc02d19
RRiverWest <kawa.ryo.0126@gmail.com>
2025-10-17 00:43:47 +0900

    feat: 各ボタンに機能割当

--- src/components/front-arms-buttons/FrontArmButtons.tsx
+++ src/components/front-arms-buttons/FrontArmButtons.tsx
@@ -7,49 +7,49 @@ const { sendMessage } = useWebSocket.getState();
 
 const CollectModeButton = () => {
   return (
-    <Button className="FrontArm" onClick={() => sendMessage({ command: "" })}>
+    <Button className="FrontArm" onClick={() => sendMessage({ command: "arm_collect_mode" })}>
       回収モード
     </Button>
   );
 };
 const HangCeilingButton = () => {
   return (
-    <Button className="FrontArm" onClick={() => sendMessage({ command: "" })}>
+    <Button className="FrontArm" onClick={() => sendMessage({ command: "arm_ceiling_deploy" })}>
       天井セット
     </Button>
   );
 };
 const FoldButton = () => {
   return (
-    <Button className="FrontArm" onClick={() => sendMessage({ command: "" })}>
-      収納　前
+    <Button className="FrontArm" onClick={() => sendMessage({ command: "all_arm_fold_lower" })}>
+      収納 前
     </Button>
   );
 };
 const RightSuctionOnButton = () => {
   return (
-    <Button className="Suction" onClick={() => sendMessage({ command: "" })}>
+    <Button className="Suction" onClick={() => sendMessage({ command: "right_arm_suction_on_off", is_on: true })}>
       吸引On 右
     </Button>
   );
 };
 const RighSuctiontOffButton = () => {
   return (
-    <Button className="Suction" onClick={() => sendMessage({ command: "" })}>
+    <Button className="Suction" onClick={() => sendMessage({ command: "right_arm_suction_on_off", is_on: false })}>
       吸引Off 右
     </Button>
   );
 };
 const LeftSuctionOnButton = () => {
   return (
-    <Button className="Suction" onClick={() => sendMessage({ command: "" })}>
+    <Button className="Suction" onClick={() => sendMessage({ command: "left_arm_suction_on_off", is_on: true })}>
       吸引On 左
     </Button>
   );
 };
 const LeftSuncitonOffButton = () => {
   return (
-    <Button className="Suction" onClick={() => sendMessage({ command: "" })}>
+    <Button className="Suction" onClick={() => sendMessage({ command: "left_arm_suction_on_off", is_on: false })}>
       吸引Off 左
     </Button>
   );
--- src/components/preset/Preset.tsx
+++ src/components/preset/Preset.tsx
@@ -6,64 +6,90 @@ import "./Preset.css";
 const { sendMessage } = useWebSocket.getState();
 
 const StartButton = () => {
-  return (
-    <Button className="Preset" onClick={() => sendMessage({ command: "" })}>
-      スタート
-    </Button>
-  );
+	return (
+		<Button
+			className="Preset"
+			onClick={() => {
+				sendMessage({ command: "all_arm_start" });
+				sendMessage({ command: "all_side_arm_open" });
+			}}
+		>
+			スタート
+		</Button>
+	);
 };
 const PylonCompletion = () => {
-  return (
-    <Button className="Preset" onClick={() => sendMessage({ command: "" })}>
-      パイロン完了
-    </Button>
-  );
+	return (
+		<Button
+			className="Preset"
+			onClick={() => {
+				sendMessage({ command: "arm_collect_mode" });
+				sendMessage({ command: "all_side_arm_fold" });
+			}}
+		>
+			パイロン完了
+		</Button>
+	);
 };
 const HangCeilingButton = () => {
-  return (
-    <Button className="Preset" onClick={() => sendMessage({ command: "" })}>
-      天井セット
-    </Button>
-  );
+	return (
+		<Button
+			className="Preset"
+			onClick={() => sendMessage({ command: "arm_ceiling_deploy" })}
+		>
+			天井セット
+		</Button>
+	);
 };
 const EndFrontArm = () => {
-  return (
-    <Button className="Preset" onClick={() => sendMessage({ command: "" })}>
-      フロントアーム終わり
-    </Button>
-  );
+	return (
+		<Button
+			className="Preset"
+			onClick={() => sendMessage({ command: "all_arm_fold_lower" })}
+		>
+			フロントアーム終わり
+		</Button>
+	);
 };
 const BoxMountCompletion = () => {
-  return (
-    <Button className="Preset" onClick={() => sendMessage({ command: "" })}>
-      ボックス設置完了
-    </Button>
-  );
+	return (
+		<Button
+			className="Preset"
+			onClick={() => {
+				sendMessage({ command: "side_arm_open_max" });
+				setTimeout(() => {
+					sendMessage({ command: "all_side_arm_fold" });
+				}, 1000);
+			}}
+		>
+			ボックス設置完了
+		</Button>
+	);
 };
 
 const Preset = () => {
-  return (
-    <div>
-      <VStack p={5}>
-        <h2>プリセット</h2>
-        <Box p={5}>
-          <StartButton />
-        </Box>
-        <Box p={5}>
-          <PylonCompletion />
-        </Box>
-        <Box p={5}>
-          <HangCeilingButton />
-        </Box>
-        <Box p={5}>
-          <EndFrontArm />
-        </Box>
-        <Box p={5}>
-          <BoxMountCompletion />
-        </Box>
-      </VStack>
-    </div>
-  );
+	return (
+		<div>
+			<VStack p={5}>
+				<h2>プリセット</h2>
+				<Box p={5}>
+					<StartButton />
+				</Box>
+				<Box p={5}>
+					<PylonCompletion />
+				</Box>
+				<Box p={5}>
+					<HangCeilingButton />
+				</Box>
+				<Box p={5}>
+					<EndFrontArm />
+				</Box>
+				<Box p={5}>
+					<BoxMountCompletion />
+				</Box>
+			</VStack>
+		</div>
+	);
 };
 
 export default Preset;
