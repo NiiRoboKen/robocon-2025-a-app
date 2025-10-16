e5f46375b3b588838cf4332639b6fbe1df08d904
RRiverWest <kawa.ryo.0126@gmail.com>
2025-10-17 02:28:22 +0900

    fix: jsonの例外処理

--- src/websocket.ts
+++ src/websocket.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import ReconnectingWebSocket from "reconnecting-websocket";
 import type { Status } from "./controller.ts";
 import { setting } from "./controller.ts";
-import type { Commands } from "./commandsType.ts";
+import type { Commands, getCommands } from "./commandsType.ts";
 // import { useModeStore } from "./hooks/useController";
 
 interface WebSocketState {
@@ -52,10 +52,10 @@ export const useWebSocket = create<WebSocketState>((set, get) => ({
 		};
 
 		socket.onmessage = (event) => {
-			console.log("Received:", event.data);
+			// console.log("Received:", event.data);
 			try {
-				const receivedData: Commands = JSON.parse(event.data); // ← ここがポイント！
-				console.log("Received:", receivedData);
+				const receivedData = JSON.parse(event.data);
+				// console.log("Received:", receivedData);
 
 				switch (receivedData.command) {
 					case "set_location":
@@ -73,17 +73,19 @@ export const useWebSocket = create<WebSocketState>((set, get) => ({
 					}
 					case "receive_success": {
 						set({ espConnecting: true });
+						break;
 					}
+					default:
+						console.log("Unknows command", receivedData.command);
+						break;
 				}
+			} catch (error) {
+				console.log("Invalid JSON received:" + event.data + error.name);
 			} finally {
-				console.log("json parse error", event.data);
+				console.log("json parse error");
 			}
-			// catch (error: unknown) {
-			// 	console.log("Invalid JSON received:"+ event.data +error.name);
-			// }
 		};
 	},
-
 	disconnect: () => {
 		const socket = get().socket;
 		if (socket && socket.readyState === WebSocket.OPEN) {
@@ -96,6 +98,7 @@ export const useWebSocket = create<WebSocketState>((set, get) => ({
 		const socket = get().socket;
 		if (socket && socket.readyState === WebSocket.OPEN) {
 			socket.send(JSON.stringify(data));
+			console.log(data);
 		} else {
 			console.warn("WebSocket not connected, cannot send:", data);
 		}
